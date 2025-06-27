085a13447a4339498f9f727e96e8b3dadaecfe6e
kwamebio <kwameagyemang73@gmail.com>
2025-06-27 21:39:51 +0000

    password hashing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
   validates :national_id, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :phone_number, presence: true, uniqueness: true
