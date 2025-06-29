b7ca52c425d17d3b65fe5f482c991a8c344dc426
kwamebio <kwameagyemang73@gmail.com>
2025-06-29 13:47:17 +0000

    login clean up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,9 +14,6 @@ class SessionsController < ApplicationController
         expired_at: Time.at(decoded["exp"]),
         ip_address: request.remote_ip,
         user_agent: user_agent_string,
-        # browser: ua.browser,
-        # os: ua.platform,
-        # device_type: ua.mobile? ? "Mobile" : "Desktop",
         last_active_at: Time.current
       )
 
@@ -29,21 +26,18 @@ class SessionsController < ApplicationController
   def admin_login
     admin = Admin.find_by(email: params[:email])
     if admin&.authenticate(params[:password])
-      token = JsonWebToken.encode(user_id: admin.id)
+      token = JsonWebToken.encode(admin_id: admin.id)
       decoded = JsonWebToken.decode(token)
 
       user_agent_string = request.user_agent
       ua = UserAgent.parse(user_agent_string)
 
       Session.create!(
-        user_id: admin.id,
+        admin_id: admin.id,
         token: token,
         expired_at: Time.at(decoded["exp"]),
         ip_address: request.remote_ip,
         user_agent: user_agent_string,
-        # browser: ua.browser,
-        # os: ua.platform,
-        # device_type: ua.mobile? ? "Mobile" : "Desktop",
         last_active_at: Time.current
       )
 
