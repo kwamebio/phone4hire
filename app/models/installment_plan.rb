ca481075a4674d0d3c4f2ef9cdd6811416688936
kwamebio <kwameagyemang73@gmail.com>
2025-07-15 13:21:23 +0000

    model rename

--- app/models/installment_plan.rb
+++ app/models/installment_plan.rb
@@ -1,4 +1,4 @@
-class InstallPlan < ApplicationRecord
+class InstallmentPlan < ApplicationRecord
   belongs_to :dealer
   acts_as_tenant(:dealer)
   enum status: { active: "active", completed: "completed", cancelled: "cancelled", defaulted: "defaulted" }
